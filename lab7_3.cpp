e9e1e5520b86f010c8a5fb60462af36e694e0d12
tie <tie.teiroom1234@gmail.com>
2024-12-05 14:21:04 +0700

    lab7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,11 +2,17 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	if(x >= 'A' && x <= 'Z'){
+		if(x == 'A'){
+			return 'Z';
+		}
+		return x-1;
+	}
+	return '0';
 }
 
 int main(){
-	//Test Case
+
 	cout << before('A') << "\n";
 	cout << before('B') << "\n";
 	cout << before('P') << "\n";
